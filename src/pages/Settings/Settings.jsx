bc6955588a8649e337d8d6be79c179e6c70a9b9d
JMwagunda <wagundamarc@gmail.com>
2024-07-19 12:53:18 +0300

    Finished on the settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,7 +1,36 @@
-
+import "./settings.css"
+import Sidebar from "../../components/Sidebar/Sidebar.jsx"
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingTitle">
+          <span className="settingsUpdateTitle">Update Your account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display:"none"}}/>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Judd" />
+          <label>Email</label>
+          <input type="email" placeholder="wagundamarc@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+        </form>
+          <button className="settingsSubmit">Update</button>
+      </div>
+      <Sidebar/> 
+    </div>
   )
 }
 
--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -7,9 +7,9 @@ const Write = () => {
           src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
           alt=""
         />
-        <form className="writeForm">
+        <form className=  "writeForm">
           <div className="writeFormGroup">
-            <label htmlFor="fileInput">
+            <label htmlFor="fileInput">      
               <i className="writeIcon fas fa-plus"></i>
             </label>
             <input id="fileInput" type="file" style={{ display: "none" }} />
